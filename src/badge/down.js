a6733b43ceba17af13ab25d1e15c360e16434732
cti1650 <cti1650@gmail.com>
2022-01-10 16:21:55 +0000

    downスクリプト追加

--- src/badge/down.js
+++ src/badge/down.js
@@ -1,5 +1,31 @@
 if(typeof(ovice) != "undefined"){
-
+  console.log(ovice)
+  const prof = ovice.getProfile();
+  const badgeList = [
+    "https://assets.ovice.io/emoji/symbols/keycap-10.png",
+    "https://assets.ovice.io/emoji/symbols/keycap-9.png",
+    "https://assets.ovice.io/emoji/symbols/keycap-8.png",
+    "https://assets.ovice.io/emoji/symbols/keycap-7.png",
+    "https://assets.ovice.io/emoji/symbols/keycap-6.png",
+    "https://assets.ovice.io/emoji/symbols/keycap-5.png",
+    "https://assets.ovice.io/emoji/symbols/keycap-4.png",
+    "https://assets.ovice.io/emoji/symbols/keycap-3.png",
+    "https://assets.ovice.io/emoji/symbols/keycap-2.png",
+    "https://assets.ovice.io/emoji/symbols/keycap-1.png",
+    "https://assets.ovice.io/emoji/symbols/keycap-0.png"
+  ];
+  if(prof){
+    console.log(prof.badge);
+    const index = badgeList.indexOf(prof.badge);
+    console.log(index);
+    if(index >= 0){
+      if(index != 10){
+        ovice.setProfile({badge:badgeList[index + 1]});
+      }
+    }else{
+      ovice.setProfile({badge:badgeList[0]});
+    }
+  }
 }else{
   alert('oVice内でのみ利用可能です。')
 }
