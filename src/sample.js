814735c02e20bee23c61f78858dc0b00390d3cdb
cti1650 <cti1650@gmail.com>
2022-01-11 20:21:52 +0900

    Update sample.js

--- src/sample.js
+++ src/sample.js
@@ -1,9 +1,16 @@
-// styleタグを作成
 var styleTag = document.createElement('style');
+styleTag.classList.add("test_panel_style");
+styleTag.innerText = `
 
-// styleタグに記載するcssを記入
-styleTag.src = '';
-
-// 作成したstyleタグを挿入
+`;
 document.getElementsByTagName('head')[0].insertAdjacentElement('beforeend', styleTag);
+
+var prevPanel = document.querySelectorAll('.test_panel,.test_panel_style');
+if(prevPanel) {
+  prevPanel.forEach(ele=>{ele.remove()})
+}
+
+var panel = document.createElement('div');
+panel.classList.add("test_panel");
+document.body.appendChild(panel);
 alert("test");
