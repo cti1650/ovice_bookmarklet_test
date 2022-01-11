22128fdcb903672ec39505acc8ed0534b49f2021
cti1650 <cti1650@gmail.com>
2022-01-11 20:02:48 +0900

    Update sample.js

--- src/sample.js
+++ src/sample.js
@@ -1,1 +1,9 @@
+// styleタグを作成
+var styleTag = document.createElement('style');
+
+// styleタグに記載するcssを記入
+styleTag.src = '';
+
+// 作成したstyleタグを挿入
+document.getElementsByTagName('head')[0].insertAdjacentElement('beforeend', styleTag);
 alert("test");
