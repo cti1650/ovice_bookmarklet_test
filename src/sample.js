1b4e92b77de78247ed1de1dc0c5b3e8f7a404040
cti1650 <cti1650@gmail.com>
2022-01-11 20:36:15 +0900

    Update sample.js

--- src/sample.js
+++ src/sample.js
@@ -1,16 +1,15 @@
-var styleTag = document.createElement('style');
-styleTag.classList.add("test_panel_style");
-styleTag.innerText = `
-
-`;
-document.getElementsByTagName('head')[0].insertAdjacentElement('beforeend', styleTag);
-
-var prevPanel = document.querySelectorAll('.test_panel,.test_panel_style');
+var prevPanel = document.querySelectorAll('.test_panel, .test_panel_tw');
 if(prevPanel) {
   prevPanel.forEach(ele=>{ele.remove()})
 }
 
+var styleTag = document.createElement('script');
+styleTag.classList.add('test_panel_tw');
+styleTag.src='https://unpkg.com/tailwindcss-jit-cdn';
+document.getElementsByTagName('head')[0].insertAdjacentElement('beforeend', styleTag);
+
 var panel = document.createElement('div');
-panel.classList.add("test_panel");
+panel.classList.add('test_panel','fixed','top-0','left-0','m-8','p-4','bg-red-500');
+panel.innerText = 'test';
 document.body.appendChild(panel);
 alert("test");
