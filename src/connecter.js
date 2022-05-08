bc738c8565774a629155b4686546286465412583
cti1650 <cti1650@gmail.com>
2022-05-08 12:29:13 +0900

    Update connecter.js

--- src/connecter.js
+++ src/connecter.js
@@ -11,8 +11,8 @@
         const screenshare_ele = document?.querySelector('#screenshare-block > div')
         if (screenshare_ele) {
             data = { ...data, ...{
-                ovice_has_screenshare: true,
-                ovice_screenshare_on:
+                hasScreenshare: true,
+                screenshareState:
                     !!screenshare_ele?.querySelector('i.bar-device-on'),
             }
             }
@@ -25,16 +25,16 @@
                         ele?.querySelector('img')?.['src']?.includes('screenshare')
                     ) {
                         data = { ...data,...{
-                            ovice_has_screenshare: true,
-                            ovice_screenshare_on:
+                            hasScreenshare: true,
+                            screenshareState:
                                 !!ele?.querySelector('i.bar-device-on'),
                         }}
                     }
                 })
             } else {
                 data = { ...data,...{
-                    ovice_has_screenshare: false,
-                    ovice_screenshare_on: false,
+                    hasScreenshare: false,
+                    screenshareState: false,
                 }}
             }
         }
@@ -42,14 +42,14 @@
         if (mic_ele) {
             if (mic_ele.querySelector('.bar-device-off')) {
                 data = { ...data,...{
-                    ovice_has_mic: true,
-                    ovice_mic_on: false,
+                    hasMic: true,
+                    micState: false,
                 }}
             }
             if (mic_ele.querySelector('.bar-device-on')) {
                 data = { ...data,...{
-                    ovice_has_mic: true,
-                    ovice_mic_on: true,
+                    hasMic: true,
+                    micState: true,
                 }}
             }
         }
